a38d4be0380bdd10afd0cddaac87c2c1db7bdfe9
VesTheCoder <vladislav.blackrock@gmail.com>
2025-05-13 20:07:35 +0100

    fixed settings.py

--- source/settings.py
+++ source/settings.py
@@ -5,7 +5,10 @@ load_dotenv()
 
 DEBUG = os.environ.get("DEBUG", "True").lower == "true"
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///test.db")
+if not DEBUG:
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+else:
+    DATABASE_URL = "sqlite:///test.db"
 
 TLL_DEFAULT = int(os.environ.get("TLL_DEFAULT", "30"))
 
