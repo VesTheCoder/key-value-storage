246cd2e39ee43829a8e97426f15ec4704e29109d
VesTheCoder <vladislav.blackrock@gmail.com>
2025-05-16 00:49:52 +0100

    updated unit tests

---
+++ source/unit_tests.py
@@ -0,0 +1,4 @@
+# I faced so many problems trying to set up non-mock real unit tests for my handlers, that I gave up.
+# But, The project is great and I'm proud of how I managed to make it.
+# Maybe I would get back to those unit tests in about 2 week and would finish them.
+# For the moment, I have other things to do.
