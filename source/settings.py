70551aea3529975e4fca9062ba136af65c99b248
VesTheCoder <vladislav.blackrock@gmail.com>
2025-05-16 15:05:46 +0100

    minor improvements

--- source/settings.py
+++ source/settings.py
@@ -7,7 +7,7 @@ DEBUG: bool = os.environ.get("DEBUG", "True") == "True"
 
 DB_URL = "sqlite+aiosqlite:///test.db" if DEBUG else os.environ.get("DB_URL")
 
-APP_HOST: str = os.environ.get("APP_HOST", "localhost")
-APP_PORT: int = int(os.environ.get("APP_PORT", "6969"))
+APP_HOST: str = "localhost" if DEBUG else os.environ.get("APP_HOST")
+APP_PORT: int = 6969 if DEBUG else int(os.environ.get("APP_PORT"))
 
 TLL_DEFAULT: int = int(os.environ.get("TLL_DEFAULT", "30"))
